8bf89eb3d747fb62a5d5e21df03bd0f96e19abf0
adityatel <154232100+adityatel@users.noreply.github.com>
2023-12-29 11:00:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['94767939688', 'telehacker', true],
+  ['94767939688', 'telehacer', true], 
   [''] 
 ] //Number of owners
 
